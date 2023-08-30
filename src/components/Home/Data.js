31525980b6118d20ba9ec8ffdad49430517a3c86
jeanbuhendwa <jeanjacquesirenge40@gmail.com>
2023-08-30 23:54:26 +0300

    Add home info

---
+++ src/components/Home/Data.js
@@ -0,0 +1,18 @@
+const Data = () => {
+  return (
+    <div className="home__data">
+      <h1 className="home__title">John Buhendwa 👋</h1>
+      <h3 className="home__subtitle">Web Developper</h3>
+      <p className="home__description">
+        lorem fsjfksvfjksf h hv wiugsiskjkjsgfkjgsvkj jsjdjdhfj uuf
+        sffofofofoagouqggugijfgurgui f ufufgulorem fsjfksvfjksf h hv
+        wiugsiskjkjsgfkjgsvkj jsjdjdhfj
+      </p>
+      <a href="#contact" className="button button--flex">
+        Hire me <i class="uil uil-message"></i>
+      </a>
+    </div>
+  );
+};
+
+export default Data;
