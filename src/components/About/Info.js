e4ac46854b0e40796839c198047012360e9acd4c
jeanbuhendwa <jeanjacquesirenge40@gmail.com>
2023-09-01 21:14:48 +0300

    Add about component

---
+++ src/components/About/Info.js
@@ -0,0 +1,19 @@
+import "./infos.css";
+const Info = () => {
+  return (
+    <div className="infos__container">
+      <div className="info__box">
+        <i class="bx bxs-briefcase"></i>
+        <h3 className="info__title">+ 3 Years</h3>
+        <span className="section__subtitle">Experience</span>
+      </div>
+      <div className="info__box">
+        <i class="bx bxs-award"></i>
+        <h3 className="info__title">+ 3 Years</h3>
+        <span className="section__subtitle">Experience</span>
+      </div>
+    </div>
+  );
+};
+
+export default Info;
