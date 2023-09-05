41bdd8bad160d66bd53822fef655db9799767e7e
jeanbuhendwa <jeanjacquesirenge40@gmail.com>
2023-09-05 13:53:41 +0300

    Update header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -24,23 +24,23 @@ const Header = () => {
               </a>
             </li>
             <li className="nav__item">
-              <a href="#skills" className="nav__link">
-                <i className="uil uil-file-alt nav__icon"></i> Skills
-              </a>
-            </li>
-            <li className="nav__item">
-              <a href="#services" className="nav__link">
-                <i className="uil uil-briefcase-alt nav__icon"></i> Services
-              </a>
-            </li>
-            <li className="nav__item">
               <a href="#portfolio" className="nav__link">
                 <i className="uil uil-scenery nav__icon"></i> Portfolio
               </a>
             </li>
             <li className="nav__item">
+              <a href="#skills" className="nav__link">
+                <i className="uil uil-file-alt nav__icon"></i> Skills
+              </a>
+            </li>
+            <li className="nav__item">
               <a href="#contact" className="nav__link">
                 <i className="uil uil-message nav__icon"></i> Contact
+              </a>
+            </li>
+            <li className="nav__item">
+              <a href="#contact" className="nav__link">
+                <i class="uil uil-sun"></i>
               </a>
             </li>
           </ul>
