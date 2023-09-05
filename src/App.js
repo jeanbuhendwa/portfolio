db2168e5688481c6f682955ec518571f521cc2fa
jeanbuhendwa <jeanjacquesirenge40@gmail.com>
2023-09-05 13:54:18 +0300

    Add portfolio

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
 import About from "./components/About/About";
+import Portfolio from "./components/Portfolio/Portfolio";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <main className="main">
         <Home />
         <About />
+        <Portfolio />
       </main>
     </>
   );
