815c0edbd080b01394bbbbbe088061ca68072486
jeanbuhendwa <jeanjacquesirenge40@gmail.com>
2023-09-06 19:32:26 +0300

    Update project card

--- src/components/Portfolio/ProjectCard.js
+++ src/components/Portfolio/ProjectCard.js
@@ -1,213 +1,38 @@
 import "./card.css";
-import projectImg from "../../Assets/Img/IMG_8207-min.jpeg";
-const ProjectCard = () => {
+
+const ProjectCard = ({ project }) => {
+  const { imgSrc, title, description, techIcons, role, githubLink, viewLink } =
+    project;
+
   return (
-    <>
-      <div className="card">
-        <div className="img__container">
-          <img src={projectImg} alt="project1" />
-          <h3 className="section__subtitle project__title">CoviStats App</h3>
+    <div className="card">
+      <div className="img__container">
+        <img src={imgSrc} alt={title} />
+        <h3 className="section__subtitle project__title">{title}</h3>
+      </div>
+      <div className="content__container">
+        <p>{description}</p>
+        <div className="tech__icons">
+          {techIcons.map((icon, index) => (
+            <i key={index} className={icon}></i>
+          ))}
         </div>
-        <div className="content__container">
-          <p>
-            CovidApp is a capstone project built with react js & redux that
-            provides information about corona virus in different countries.
-          </p>
-          <div className="tech__icons">
-            <i class="uil uil-github"></i>
-            <i class="uil uil-react"></i>
-            <i class="uil uil-css3-simple"></i>
-            <i class="uil uil-java-script"></i>
-            <i class="bx bxl-bootstrap"></i>
-            <i class="bx bxl-redux"></i>
+        <div className="footer__card">
+          <div className="project__role">
+            <i className="bx bx-code-alt"></i>
+            <span className="project__role-text">{role}</span>
           </div>
-          <div className="footer__card">
-            <div className="project__role">
-              <i class="bx bx-code-alt"></i>
-              <span className="project__role-text">Front-End Developer</span>
-            </div>
-            <div className="project__btn">
-              <a href="#" className="btn">
-                <i class="uil uil-github-alt"></i>
-              </a>
-              <a href="#" className="btn">
-                <i class="uil uil-eye"></i>
-              </a>
-            </div>
+          <div className="project__btn">
+            <a href={githubLink} className="btn" target="_blank">
+              <i className="uil uil-github-alt"></i>
+            </a>
+            <a href={viewLink} className="btn" target="_blank">
+              <i className="uil uil-eye"></i>
+            </a>
           </div>
         </div>
       </div>
-      <div className="card">
-        <div className="img__container">
-          <img src={projectImg} alt="project1" />
-          <h3 className="section__subtitle project__title">CoviStats App</h3>
-        </div>
-        <div className="content__container">
-          <p>
-            CovidApp is a capstone project built with react js & redux that
-            provides information about corona virus in different countries.
-          </p>
-          <div className="tech__icons">
-            <i class="uil uil-github"></i>
-            <i class="uil uil-react"></i>
-            <i class="uil uil-css3-simple"></i>
-            <i class="uil uil-java-script"></i>
-            <i class="bx bxl-bootstrap"></i>
-            <i class="bx bxl-redux"></i>
-          </div>
-          <div className="footer__card">
-            <div className="project__role">
-              <i class="bx bx-code-alt"></i>
-              <span className="project__role-text">Front-End Developer</span>
-            </div>
-            <div className="project__btn">
-              <a href="#" className="btn">
-                <i class="uil uil-github-alt"></i>
-              </a>
-              <a href="#" className="btn">
-                <i class="uil uil-eye"></i>
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="card">
-        <div className="img__container">
-          <img src={projectImg} alt="project1" />
-          <h3 className="section__subtitle project__title">CoviStats App</h3>
-        </div>
-        <div className="content__container">
-          <p>
-            CovidApp is a capstone project built with react js & redux that
-            provides information about corona virus in different countries.
-          </p>
-          <div className="tech__icons">
-            <i class="uil uil-github"></i>
-            <i class="uil uil-react"></i>
-            <i class="uil uil-css3-simple"></i>
-            <i class="uil uil-java-script"></i>
-            <i class="bx bxl-bootstrap"></i>
-            <i class="bx bxl-redux"></i>
-          </div>
-          <div className="footer__card">
-            <div className="project__role">
-              <i class="bx bx-code-alt"></i>
-              <span className="project__role-text">Front-End Developer</span>
-            </div>
-            <div className="project__btn">
-              <a href="#" className="btn">
-                <i class="uil uil-github-alt"></i>
-              </a>
-              <a href="#" className="btn">
-                <i class="uil uil-eye"></i>
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="card">
-        <div className="img__container">
-          <img src={projectImg} alt="project1" />
-          <h3 className="section__subtitle project__title">CoviStats App</h3>
-        </div>
-        <div className="content__container">
-          <p>
-            CovidApp is a capstone project built with react js & redux that
-            provides information about corona virus in different countries.
-          </p>
-          <div className="tech__icons">
-            <i class="uil uil-github"></i>
-            <i class="uil uil-react"></i>
-            <i class="uil uil-css3-simple"></i>
-            <i class="uil uil-java-script"></i>
-            <i class="bx bxl-bootstrap"></i>
-            <i class="bx bxl-redux"></i>
-          </div>
-          <div className="footer__card">
-            <div className="project__role">
-              <i class="bx bx-code-alt"></i>
-              <span className="project__role-text">Front-End Developer</span>
-            </div>
-            <div className="project__btn">
-              <a href="#" className="btn">
-                <i class="uil uil-github-alt"></i>
-              </a>
-              <a href="#" className="btn">
-                <i class="uil uil-eye"></i>
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="card">
-        <div className="img__container">
-          <img src={projectImg} alt="project1" />
-          <h3 className="section__subtitle project__title">CoviStats App</h3>
-        </div>
-        <div className="content__container">
-          <p>
-            CovidApp is a capstone project built with react js & redux that
-            provides information about corona virus in different countries.
-          </p>
-          <div className="tech__icons">
-            <i class="uil uil-github"></i>
-            <i class="uil uil-react"></i>
-            <i class="uil uil-css3-simple"></i>
-            <i class="uil uil-java-script"></i>
-            <i class="bx bxl-bootstrap"></i>
-            <i class="bx bxl-redux"></i>
-          </div>
-          <div className="footer__card">
-            <div className="project__role">
-              <i class="bx bx-code-alt"></i>
-              <span className="project__role-text">Front-End Developer</span>
-            </div>
-            <div className="project__btn">
-              <a href="#" className="btn">
-                <i class="uil uil-github-alt"></i>
-              </a>
-              <a href="#" className="btn">
-                <i class="uil uil-eye"></i>
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className="card">
-        <div className="img__container">
-          <img src={projectImg} alt="project1" />
-          <h3 className="section__subtitle project__title">CoviStats App</h3>
-        </div>
-        <div className="content__container">
-          <p>
-            CovidApp is a capstone project built with react js & redux that
-            provides information about corona virus in different countries.
-          </p>
-          <div className="tech__icons">
-            <i class="uil uil-github"></i>
-            <i class="uil uil-react"></i>
-            <i class="uil uil-css3-simple"></i>
-            <i class="uil uil-java-script"></i>
-            <i class="bx bxl-bootstrap"></i>
-            <i class="bx bxl-redux"></i>
-          </div>
-          <div className="footer__card">
-            <div className="project__role">
-              <i class="bx bx-code-alt"></i>
-              <span className="project__role-text">Front-End Developer</span>
-            </div>
-            <div className="project__btn">
-              <a href="#" className="btn">
-                <i class="uil uil-github-alt"></i>
-              </a>
-              <a href="#" className="btn">
-                <i class="uil uil-eye"></i>
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
-    </>
+    </div>
   );
 };
 
