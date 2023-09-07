ee7e5b8aef0059aa1b5ba8d126bb22b9d9ba1a49
jeanbuhendwa <jeanjacquesirenge40@gmail.com>
2023-09-07 22:19:23 +0300

    Add skills

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Header from "./components/Header/Header";
 import Home from "./components/Home/Home";
 import About from "./components/About/About";
 import Portfolio from "./components/Portfolio/Portfolio";
+import Skills from "./components/Skills/Skills";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Home />
         <About />
         <Portfolio />
+        <Skills />
       </main>
     </>
   );
