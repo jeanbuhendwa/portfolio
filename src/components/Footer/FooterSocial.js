c269f3530e0325b5f85bf5ad290318be14408671
jeanbuhendwa <jeanjacquesirenge40@gmail.com>
2023-09-10 19:36:53 +0300

    Add footer component

---
+++ src/components/Footer/FooterSocial.js
@@ -0,0 +1,19 @@
+const FooterSocial = () => {
+  return (
+    <div className="footer__social">
+      <a href="#" className="footer__social-icon" target="_blank">
+        <i class="uil uil-github"></i>
+      </a>
+      <a href="#" className="footer__social-icon" target="_blank">
+        <i class="uil uil-linkedin"></i>
+      </a>
+      <a href="#" className="footer__social-icon" target="_blank">
+        <i class="uil uil-twitter"></i>
+      </a>
+      <a href="#" className="footer__social-icon" target="_blank">
+        <i class="uil uil-medium-m"></i>
+      </a>
+    </div>
+  );
+};
+export default FooterSocial;
