8707862f27c0d4c2fd8bdd943bdc0bf7969eb794
jeanbuhendwa <jeanjacquesirenge40@gmail.com>
2023-09-11 18:54:56 +0300

    Update footer links

--- src/components/Footer/FooterSocial.js
+++ src/components/Footer/FooterSocial.js
@@ -1,16 +1,32 @@
 const FooterSocial = () => {
   return (
     <div className="footer__social">
-      <a href="#" className="footer__social-icon" target="_blank">
+      <a
+        href="https://github.com/jeanbuhendwa"
+        className="footer__social-icon"
+        target="_blank"
+      >
         <i class="uil uil-github"></i>
       </a>
-      <a href="#" className="footer__social-icon" target="_blank">
+      <a
+        href="https://www.linkedin.com/in/johnbuhendwa/"
+        className="footer__social-icon"
+        target="_blank"
+      >
         <i class="uil uil-linkedin"></i>
       </a>
-      <a href="#" className="footer__social-icon" target="_blank">
+      <a
+        href="https://twitter.com/JeanjacqueI"
+        className="footer__social-icon"
+        target="_blank"
+      >
         <i class="uil uil-twitter"></i>
       </a>
-      <a href="#" className="footer__social-icon" target="_blank">
+      <a
+        href="https://medium.com/@jeanjacquesirenge40"
+        className="footer__social-icon"
+        target="_blank"
+      >
         <i class="uil uil-medium-m"></i>
       </a>
     </div>
